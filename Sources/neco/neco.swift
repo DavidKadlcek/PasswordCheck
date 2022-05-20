410fa1f89a3f0e110de93f2d165dbe5332d570e1
David Kadlček <david.kadlcek@matee.cz>
2022-05-20 14:30:52 +0200

    Update Package

---
+++ Sources/neco/neco.swift
@@ -0,0 +1,6 @@
+public struct neco {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/necoTests/necoTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import neco
+
+final class necoTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(neco().text, "Hello, World!")
+    }
+}
